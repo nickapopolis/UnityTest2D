ff501f381f6eb63c614fbfac842f1bd47c8faa74
Nick Tierney <nick.tierney@shopify.com>
2020-03-28 12:06:52 -0400

    Initial commit

---
+++ Assets/CameraFollowPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+    public Player player;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3 (player.position().x, player.position().y, transform.position.z);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+
+	private Rigidbody2D rb;
+	private Vector2 direction = Vector2.one;
+	public float speed = 6f;
+	
+	private void Start () 
+	{
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void FixedUpdate () {
+
+		direction.x = Input.GetAxisRaw ("Horizontal");
+		direction.y = Input.GetAxisRaw ("Vertical");
+
+		rb.velocity = direction * speed;
+	
+	}
+
+	public Vector2 position(){
+		return rb.position;
+	}
+}
